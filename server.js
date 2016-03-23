28dc141e510f7358c2d3be0d85fb2ef69a29b459
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2016-03-23 22:58:05 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+function getCollection(done) {
+    MongoClient.connect(process.env.MONGODB_URL || 'mongodb://localhost/urba', function (err, db) {
+        if (err) return done(err);
+        console.log('Connected to database');
+        db.collection('servitudes', done);
+    });
+}
+
+getCollection(function (err, servColl) {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+
+    const app = express();
+
+    app.use(cors());
+    app.use(bodyParser.json());
+
+    // Inject collection
+    app.use(function (req, res, next) {
+        req.servColl = servColl;
+        next();
+    });
+
+    app.post('/servitudes', function (req, res, next) {
+        servColl
+            .find({ geometry: { $geoIntersects: { $geometry: req.body.geom } } })
+            .project({ geometry: 0 })
+            .toArray(function (err, servitudes) {
+                if (err) return next(err);
+                res.send(servitudes.map(servitude => ({
+                    nom: servitude.properties.libelle,
+                    type: 'AC1',
+                    codeMerimee: servitude.properties.cd_merimee,
+                    libelleCommune: servitude.properties.lb_com,
+                    id: servitude.properties.id_obj
+                })));
+            });
+    });
+
+    const listenPort = process.env.PORT || 5000;
+
+    app.listen(listenPort, function () {
+        console.log('Start listening on port ' + listenPort);
+    });
+})
