791a916fcbf483fb8bca021094da49daf8e9af3c
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2016-04-04 18:41:49 +0200

    Import generateur and add details API

--- server.js
+++ server.js
@@ -1,53 +1,47 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const getCollection = require('./lib/mongodb').getCollection;
 
-function getCollection(done) {
-    MongoClient.connect(process.env.MONGODB_URL || 'mongodb://localhost/urba', function (err, db) {
-        if (err) return done(err);
-        console.log('Connected to database');
-        db.collection('servitudes', function (err, servColl) {
-            if (err) return done(err);
-            servColl.createIndex({ assiette: '2dsphere' }, function (err, result) {
-                if (err) return done(err);
-                console.log(`Index created: ${result}`)
-                done(null, servColl);
-            });
+getCollection()
+    .then(servColl => {
+        const app = express();
+
+        app.use(cors());
+        app.use(bodyParser.json());
+
+        // Inject collection
+        app.use(function (req, res, next) {
+            req.servColl = servColl;
+            next();
         });
-    });
-}
 
-getCollection(function (err, servColl) {
-    if (err) {
+        app.post('/servitudes', function (req, res, next) {
+            servColl
+                .find({ assiette: { $geoIntersects: { $geometry: req.body.geom } } })
+                .project({ assiette: 0, generateur: 0 })
+                .toArray(function (err, servitudes) {
+                    if (err) return next(err);
+                    res.send(servitudes);
+                });
+        });
+
+        app.get('/servitudes/:servId', function (req, res, next) {
+            servColl
+                .findOne({ _id: ObjectID.createFromHexString(req.params.servId) }, function (err, servitude) {
+                    if (err) return next(err);
+                    res.send(servitude);
+                });
+        });
+
+        const listenPort = process.env.PORT || 5000;
+
+        app.listen(listenPort, function () {
+            console.log('Start listening on port ' + listenPort);
+        });
+    })
+    .catch(err => {
         console.error(err);
         process.exit(1);
-    }
-
-    const app = express();
-
-    app.use(cors());
-    app.use(bodyParser.json());
-
-    // Inject collection
-    app.use(function (req, res, next) {
-        req.servColl = servColl;
-        next();
     });
-
-    app.post('/servitudes', function (req, res, next) {
-        servColl
-            .find({ assiette: { $geoIntersects: { $geometry: req.body.geom } } })
-            .project({ assiette: 0 })
-            .toArray(function (err, servitudes) {
-                if (err) return next(err);
-                res.send(servitudes);
-            });
-    });
-
-    const listenPort = process.env.PORT || 5000;
-
-    app.listen(listenPort, function () {
-        console.log('Start listening on port ' + listenPort);
-    });
-})
--- transform.js
+++ transform.js
@@ -1,26 +1,77 @@
 const JSONStream = require('JSONStream');
-const map = require("through2-map")
-const fs = require('fs');
+const map = require('through2-map')
 const request = require('request');
+const _ = require('lodash');
+const ServitudeWriter = require('./lib/ServitudeWriter');
 
-const parser = JSONStream.parse('features.*');
+const datasets = {
+    generateur: {
+        resourceId: 'services/55673a34330f1fcd4832db30/feature-types/monuments_historiques_immeuble',
+        source: 'passerelle',
+        key: 'codeMerimee',
+        mapping: {
+            nom: 'properties.libelle',
+            codeMerimee: 'properties.cd_merimee',
+            generateur: 'geometry',
+        },
+        set: {
+            type: 'AC1',
+        },
+    },
+    assiette: {
+        resourceId: 'services/55673a34330f1fcd4832db30/feature-types/monuments_historiques_perimetr',
+        source: 'passerelle',
+        key: 'codeMerimee',
+        mapping: {
+            nom: 'properties.libelle',
+            codeMerimee: 'properties.cd_merimee',
+            libelleCommune: 'properties.lb_com',
+            assiette: 'geometry',
+        },
+        set: {
+            type: 'AC1',
+        },
+    },
+};
 
-const src = request({
-    url: 'https://inspire.data.gouv.fr/api/geogw/services/55673a34330f1fcd4832db30/feature-types/monuments_historiques_perimetr/download',
-    qs: { format: 'GeoJSON', projection: 'WGS84' }
+function getPasserelleRequest(resourceId) {
+    return request({
+        url: `https://inspire.data.gouv.fr/api/geogw/${resourceId}/download`,
+        qs: { format: 'GeoJSON', projection: 'WGS84' }
+    });
+}
+
+function getParser() {
+    return JSONStream.parse('features.*');
+}
+
+function getServitudeWriter(key) {
+    return new ServitudeWriter(key);
+}
+
+function importDataset(dataset, done) {
+    getPasserelleRequest(dataset.resourceId)
+        .pipe(getParser())
+        .pipe(map.obj(row => {
+            const transformedRow = {};
+            _.forEach(dataset.mapping, (mappingDef, attrName) => {
+                const val = _.get(row, mappingDef);
+                if (val) transformedRow[attrName] = val;
+            });
+            _.forEach(dataset.set, (val, attrName) => {
+                transformedRow[attrName] = val;
+            });
+            return transformedRow;
+        }))
+        .pipe(getServitudeWriter(dataset.key))
+        .on('finish', () => {
+            done();
+        });
+}
+
+importDataset(datasets.assiette, err => {
+    importDataset(datasets.generateur, err => {
+        console.log('Import terminé');
+        process.exit(0);
+    });
 });
-
-const dest = fs.createWriteStream(__dirname + '/data/servitudes_ac1.json');
-
-src
-    .pipe(parser)
-    .pipe(map.obj(servitudeGeoJSON => ({
-        nom: servitudeGeoJSON.properties.libelle,
-        type: 'AC1',
-        codeMerimee: servitudeGeoJSON.properties.cd_merimee,
-        libelleCommune: servitudeGeoJSON.properties.lb_com,
-        id: servitudeGeoJSON.properties.id_obj,
-        assiette: servitudeGeoJSON.geometry
-    })))
-    .pipe(JSONStream.stringify())
-    .pipe(dest);
