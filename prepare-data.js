26fcf4b376911525d615ca688020f018a1a2b2bb
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2016-04-11 16:45:16 +0200

    Add more sources

--- prepare-data.js
+++ prepare-data.js
@@ -16,35 +16,35 @@ const combine = require('stream-combiner');
 
 const datasets = [
     // Midi-Pyrénées
-    // {
-    //     coverage: ['dep09', 'dep12', 'dep21', 'dep32', 'dep46', 'dep65', 'dep81', 'dep82'],
-    //     resourceId: 'services/55673a34330f1fcd4832db30/feature-types/monuments_historiques_immeuble',
-    //     source: 'passerelle',
-    //     key: 'codeMerimee',
-    //     mapping: {
-    //         nom: 'properties.libelle',
-    //         codeMerimee: 'properties.cd_merimee',
-    //         generateur: 'geometry',
-    //     },
-    //     set: {
-    //         type: 'AC1',
-    //     },
-    // },
-    // {
-    //     coverage: ['dep09', 'dep12', 'dep21', 'dep32', 'dep46', 'dep65', 'dep81', 'dep82'],
-    //     resourceId: 'services/55673a34330f1fcd4832db30/feature-types/monuments_historiques_perimetr',
-    //     source: 'passerelle',
-    //     key: 'codeMerimee',
-    //     mapping: {
-    //         nom: 'properties.libelle',
-    //         codeMerimee: 'properties.cd_merimee',
-    //         libelleCommune: 'properties.lb_com',
-    //         assiette: 'geometry',
-    //     },
-    //     set: {
-    //         type: 'AC1',
-    //     },
-    // },
+    {
+        coverage: ['dep09', 'dep12', 'dep21', 'dep32', 'dep46', 'dep65', 'dep81', 'dep82'],
+        resourceId: 'services/55673a34330f1fcd4832db30/feature-types/monuments_historiques_immeuble',
+        source: 'passerelle',
+        key: 'codeMerimee',
+        mapping: {
+            nom: 'properties.libelle',
+            codeMerimee: 'properties.cd_merimee',
+            generateur: 'geometry',
+        },
+        set: {
+            type: 'AC1',
+        },
+    },
+    {
+        coverage: ['dep09', 'dep12', 'dep21', 'dep32', 'dep46', 'dep65', 'dep81', 'dep82'],
+        resourceId: 'services/55673a34330f1fcd4832db30/feature-types/monuments_historiques_perimetr',
+        source: 'passerelle',
+        key: 'codeMerimee',
+        mapping: {
+            nom: 'properties.libelle',
+            codeMerimee: 'properties.cd_merimee',
+            libelleCommune: 'properties.lb_com',
+            assiette: 'geometry',
+        },
+        set: {
+            type: 'AC1',
+        },
+    },
     // Aquitaine Limousin Poitou Charentes
     {
         coverage: ['dep16'],
@@ -76,12 +76,85 @@ const datasets = [
             codeCommune: 'properties.Insee',
             niveauProtection: 'properties.Type',
             generateur: 'geometry',
+            codeLocal: 'properties.Code',
+        },
+        set: {
+            type: 'AC1',
+        },
+    },
+    {
+        coverage: ['dep45'],
+        resourceId: 'file-packages/7731d6d7961499bf08d9a93a24235b6fc7f4b8ee/AC1_ASSIETTE_SUP_S_045',
+        source: 'passerelle',
+        decode: 'win1252',
+        key: 'codeLocal',
+        mapping: {
+            codeLocal: 'properties.Code',
+            assiette: 'geometry',
+        },
+        set: {
+            type: 'AC1',
+        },
+    },
+    // Hauts-de-France
+    {
+        coverage: ['dep80'],
+        resourceId: 'file-packages/bec1458249a09f7e1346b5823418c5f195b59456/N_AC1_ASSIETTE_SUP_S_080',
+        source: 'passerelle',
+        decode: 'win1252',
+        key: 'codeLocal',
+        mapping: {
+            nom: 'properties.Libelle',
+            codeLocal: 'properties.NOM_ASS',
+            codeCommune: 'properties.INSEE',
+            libelleCommune: 'properties.Commune',
+            assiette: 'geometry',
         },
         set: {
             type: 'AC1',
         },
     },
     // Grand Est
+    {
+        coverage: ['dep67'],
+        resourceId: 'file-packages/975b57413655249ff7f7437f937e7d16b0d90bc3/N_MONUMENT_HISTO_P_067.TAB',
+        source: 'passerelle',
+        decode: 'win1252',
+        key: 'codeMerimee',
+        filters: ['computeAssietteAC1'],
+        mapping: {
+            nom: 'properties.NOM',
+            adresse: 'properties.ADRESSE',
+            codeMerimee: 'properties.ID_MERIME',
+            libelleCommune: 'properties.COMMUNE',
+            codeCommune: 'properties.N_INSEE',
+            niveauProtection: 'properties.TYPE_JURIDIQUE',
+            generateur: 'geometry',
+        },
+        set: {
+            type: 'AC1',
+        },
+    },
+    {
+        coverage: ['dep67'],
+        resourceId: 'file-packages/e5be2f75b6cf2115ce4eea12005fb065fea9af88/N_MONUMENT_HISTO_S_067.TAB',
+        source: 'passerelle',
+        decode: 'win1252',
+        key: 'codeMerimee',
+        filters: ['computeAssietteAC1'],
+        mapping: {
+            nom: 'properties.NOM',
+            adresse: 'properties.ADRESSE',
+            codeMerimee: 'properties.ID_MERIME',
+            libelleCommune: 'properties.COMMUNE',
+            codeCommune: 'properties.N_INSEE',
+            niveauProtection: 'properties.TYPE_JURIDIQUE',
+            generateur: 'geometry',
+        },
+        set: {
+            type: 'AC1',
+        },
+    },
     {
         coverage: ['dep68'],
         resourceId: 'file-packages/9387da9ba5d2c583ca64286d3c262381db4ff28b/N_MONUMENT_HISTO_S_068',
@@ -102,6 +175,25 @@ const datasets = [
             type: 'AC1',
         },
     },
+    {
+        coverage: ['dep68'],
+        resourceId: 'file-packages/c15b9576bcb997b725df14919b188d4d80f8d793/N_AC1_ASSIETTE_SUP_S_068.shp',
+        source: 'passerelle',
+        decode: 'win1252',
+        key: 'codeMerimee',
+        mapping: {
+            nom: 'properties.Immeuble',
+            adresse: 'properties.Adresse',
+            codeMerimee: 'properties.Ref_merim',
+            libelleCommune: 'properties.Commune',
+            codeCommune: 'properties.Insee',
+            niveauProtection: 'properties.Protection',
+            assiette: 'geometry',
+        },
+        set: {
+            type: 'AC1',
+        },
+    },
     // Auvergne Rhône Alpes
     {
         coverage: ['dep73'],
@@ -114,6 +206,40 @@ const datasets = [
             codeMerimee: 'properties.REF_MERIM',
             libelleCommune: 'properties.COMMUNE',
             generateur: 'geometry',
+        },
+        set: {
+            type: 'AC1',
+        },
+    },
+    {
+        coverage: ['dep15'],
+        resourceId: 'file-packages/709aa4e2c71571b93e473dc8a3ee0b5ec04a3c1c/L_MONUMENTS_HISTO_P_015.TAB',
+        source: 'passerelle',
+        decode: 'win1252',
+        key: 'codeLocal',
+        filters: ['computeAssietteAC1'],
+        mapping: {
+            nom: 'properties.nom',
+            codeLocal: 'properties.cod_let',
+            codeCommune: 'properties.codcom',
+            generateur: 'geometry',
+        },
+        set: {
+            type: 'AC1',
+        },
+    },
+    {
+        coverage: ['dep15'],
+        resourceId: 'file-packages/ac43c64619b009943ffead414c5f93c041b15889/L_PERIMPROTECTIONMH_015.TAB',
+        source: 'passerelle',
+        decode: 'win1252',
+        key: 'codeLocal',
+        mapping: {
+            nom: 'properties.Nom',
+            codeLocal: 'properties.Cod_Let',
+            codeCommune: 'properties.CodCom',
+            libelleCommune: 'properties.Commune',
+            assiette: 'geometry',
         },
         set: {
             type: 'AC1',
